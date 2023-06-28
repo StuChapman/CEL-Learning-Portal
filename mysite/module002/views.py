66e7078c24caa78eee747d414a922d8b23bba4b7
Stuart Chapman <chapman.stuart@sky.com>
2023-06-28 16:18:35 +0000

    Added module002 test views

--- mysite/module002/views.py
+++ mysite/module002/views.py
@@ -363,16 +363,16 @@ def page010module002(request):
         'prev_url': 'page009module002',
         'prev_page': 'damp & mould survey',
         'prev_page_small': 'survey',
-        'next_url': 'testintro',
+        'next_url': 'damptestintro',
         'next_page': 'test',
         'next_page_small': 'test',
     }
     return render(request, 'dampandmould/page010.html', context)
 
 
-def testintro(request):
-    """ A view to return the testintro page """
-    thistest = 'testintro'
+def damptestintro(request):
+    """ A view to return the damptestintro page """
+    thistest = 'damptestintro'
     nexttest = 'test001module002'
     context = {
         'thistest': thistest,
@@ -382,3 +382,202 @@ def testintro(request):
     }
 
     return render(request, 'dampandmould/testintro.html', context)
+
+
+def dampcheckanswer(request):
+    """ Check the answer to each question against the correct answer """
+    nexthidden = 'false'
+    test_already_complete = 'false'
+    next_url = 'nexttest'
+    next_page = 'next question'
+
+    if request.user.is_authenticated:
+
+        if request.GET:
+            if 'test_list' in request.GET:
+                test_list = request.GET['test_list']
+                test_list_list = test_list.split(',')
+                test_already_complete_flag = test_list_list[2]
+                thistest = test_list_list[0]
+                next_test = get_object_or_404(Answers,
+                                              test=thistest)
+                nexttest = next_test.nexttest
+                if test_already_complete_flag == 'true':
+                    nexttemplate = 'dampandmould/' + nexttest[:7] + '.html'
+                else:
+                    nexttemplate = 'dampandmould/' + thistest[:7] + '.html'
+
+        """ check if a Test Result exists for this user for this test """
+        tests = Tests.objects.all()
+        test_exists = (tests.filter
+                       (user=request.user,
+                        test=thistest))
+        if test_exists:
+            test_already_complete = 'true'
+            if thistest == 'test010':
+                next_page = 'test summary'
+            else:
+                next_page = 'next question'
+        else:
+            """ get information from testanswer form """
+            if request.POST:
+                testanswer = request.POST['testanswer']
+                """ check if Test Result matches correct answer """
+                correct_answer_query = get_object_or_404(Answers,
+                                                         test=thistest)
+                correct_answer = correct_answer_query.correctanswer
+                if testanswer == correct_answer:
+                    result = 1
+                else:
+                    result = 0
+
+                """ create a Test Result for this user for this test """
+                user_test = Tests(user=request.user,
+                                  test=thistest,
+                                  status=1,
+                                  answer=testanswer,
+                                  result=result)
+                user_test.save()
+                test_already_complete = 'true'
+            nexthidden = 'false'
+            if thistest == 'test010':
+                next_page = 'test summary'
+            else:
+                next_page = 'next question'
+
+    context = {
+        'thistest': thistest,
+        'nexttest': nexttest,
+        'arrows': 'arrows',
+        'nexthidden': nexthidden,
+        'test_already_complete': test_already_complete,
+        'next_url': next_url,
+        'next_page': next_page,
+        'next_page_small': next_page,
+    }
+    return render(request, nexttemplate, context)
+
+
+def dampnexttest(request):
+    """ Navigate to the next test """
+    nexthidden = 'false'
+    test_already_complete = 'false'
+    next_url = 'dampcheckanswer'
+
+    if request.user.is_authenticated:
+
+        if request.GET:
+            if 'test_list' in request.GET:
+                test_list = request.GET['test_list']
+                test_list_list = test_list.split(',')
+                test_already_complete_flag = test_list_list[2]
+                thistest = test_list_list[1]
+                next_test = get_object_or_404(Answers,
+                                              test=thistest)
+                nexttest = next_test.nexttest
+                nexttemplate = 'dampandmould/' + thistest[:7] + '.html'
+
+        """ check if a Test Result exists for this user for this test """
+        tests = Tests.objects.all()
+        test_exists = (tests.filter
+                       (user=request.user,
+                        test=thistest))
+        if test_exists:
+            test_already_complete = 'true'
+            nexthidden = 'false'
+            if thistest == 'test010':
+                next_page = 'test summary'
+            else:
+                next_page = 'next question'
+            next_url = 'dampnexttest'
+        else:
+            test_already_complete = 'false'
+            test_already_complete_flag == 'false'
+            nexthidden = 'true'
+            next_page = 'submit'
+
+        if thistest == 'testsummary':
+            """ get the Test Result for this user for this module """
+            tests = Tests.objects.all()
+            test_result = (tests.filter
+                           (user=request.user))
+            failed_test_result = test_result.filter(test__contains='')
+            this_test_result = failed_test_result.exclude(test__contains='fail').order_by('test')
+            this_test_count = this_test_result.count()
+            test_score = this_test_result.filter(result='1').count()
+
+            context = {
+                'this_test_result': this_test_result,
+                'this_test_count': this_test_count,
+                'test_score': test_score,
+            }
+
+            return render(request, 'dampandmould/testsummary.html', context)
+
+    context = {
+        'thistest': thistest,
+        'nexttest': nexttest,
+        'arrows': 'arrows',
+        'nexthidden': nexthidden,
+        'test_already_complete': test_already_complete,
+        'next_url': next_url,
+        'next_page': next_page,
+        'next_page_small': next_page,
+    }
+    return render(request, nexttemplate, context)
+
+
+def dampretest(request):
+    """ A view to reset the incorrect tests and return the retest page """
+    thistest = 'testintro'
+    nexttest = 'test001'
+
+    if request.user.is_authenticated:
+
+        if request.GET:
+            if 'module_list' in request.GET:
+                module_list = request.GET['module_list']
+
+                """ create a Failed Test List for this user for this module """
+                tests = Tests.objects.all()
+                test_result = (tests.filter
+                               (user=request.user))
+                failed_test_result = test_result.filter(test__contains='')
+                this_test_result = failed_test_result.exclude(test__contains='fail')
+                this_failed_test_list = this_test_result.filter(result='0').order_by('test')
+                for test in this_failed_test_list:
+                    test.test = test.test + 'fail'
+                    test.save()
+
+    context = {
+        'thistest': thistest,
+        'nexttest': nexttest,
+        'arrows': 'noarrows',
+        'nexthidden': 'false',
+        'this_failed_test_list': this_failed_test_list,
+    }
+
+    return render(request, 'dampandmould/testintro.html', context)
+
+
+def damptestcertificate(request):
+    """ A view to return the damptestcertificate page """
+    if request.user.is_authenticated:
+
+        if request.GET:
+            """ get the Test Result for this user for this module """
+            tests = Tests.objects.all()
+            test_result = (tests.filter
+                           (user=request.user))
+            failed_test_result = test_result.filter(test__contains='')
+            this_test_result = failed_test_result.exclude(test__contains='fail').order_by('test')
+            this_test_count = this_test_result.count()
+            test_score = this_test_result.filter(result='1').count()
+
+    context = {
+        'this_test_result': this_test_result,
+        'this_test_count': this_test_count,
+        'test_score': test_score,
+    }
+
+    return render(request, 'dampandmould/testcertificate.html', context)
