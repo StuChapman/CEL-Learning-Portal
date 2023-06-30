f1ce1296fe7bd28175676dcb4573f5fc0c44d0aa
Stuart Chapman <chapman.stuart@sky.com>
2023-06-30 12:27:39 +0000

    Added passwordReset function

--- mysite/static/js/modals.js
+++ mysite/static/js/modals.js
@@ -29,6 +29,36 @@ function sendEmail() {
 
 }
 
+function passwordReset() {
+
+    if (!(/^\S+@\S+\.\S+$/.test(this.passwordemail.value))) {
+        alert('Please enter a valid email address.');
+        this.passwordemail.focus();
+        return;
+    }
+
+    if (!(/^[a-z A-Z?:,.-]+$/.test(this.passwordmessage.value)) ||this.passwordmessage.value.length < 10) {
+        alert('Please enter a question of at least 10 characters, and no special characters .');
+        this.passwordmessage.focus();
+        return false;
+    }
+    
+    emailjs.init("user_37585cYmkMNZRiOobd27i");
+    
+    var template_params = {
+    "from_email": "From: " + this.passwordemail.value,
+    "question": "Question: " + this.passwordmessage.value
+    };
+    
+    var service_id = "continuous_engagement";
+    var template_id = "template_6DMLrcJu";
+
+    emailjs.send(service_id, template_id, template_params);
+    
+    alert('Email sent succesfully.');
+
+}
+
 // Function: navigate to Continuous Engagament Ltd homepage //
 function goCELHome() {
     window.location.replace('https://continuous-engagement.co.uk');
